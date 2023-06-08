7e83522bf8753944c7a87234a3847285f5c426b2
선형채 <sunblock99@kakao.com>
2023-06-08 09:31:13 +0900

    테스트코드 커밋

--- demo/src/main/java/com/example/demo/testController.java
+++ demo/src/main/java/com/example/demo/testController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class testController {
 
-	@GetMapping("/")
+	@GetMapping("/test")
 	public String testtest() {
-		
+		System.out.println("테스트 컨트롤러 호출");
 		return "test";
 	}
 }
