16746f883f6e80337bd279f813bf42294d9b8f30
선형채 <sunblock99@kakao.com>
2023-07-24 14:34:06 +0900

    에러의 연속

--- src/main/java/com/example/demo/testController.java
+++ src/main/java/com/example/demo/testController.java
@@ -6,14 +6,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class testController {
 
-	@RequestMapping("/test")
+	@GetMapping("/test")
 	public String testtest() {
 		System.out.println("테스트 컨트롤러 호출");
 		return "test";
 	}
 
 	
-	@RequestMapping("/sunblock")
+	@GetMapping("/sunblock")
 	public String testtest2() {
 		System.out.println("저는 sunblock 호출입니다.");
 		return "test";
